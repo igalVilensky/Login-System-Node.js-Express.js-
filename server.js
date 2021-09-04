3d398e11eed21cb1e78a05303cf8013611619570
igalv <vilenskyigal@gmail.com>
2021-09-04 18:35:42 +0200

    middlewares, logout, passport, flash, session

--- server.js
+++ server.js
@@ -1,35 +1,70 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const http = require("http");
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-//  const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({ extended: false }));
-app.set("view-engine", "ejs");
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 const users = [];
 
+//  const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
 // Main Page [ http://localhost:3000 ]
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Igal" });
+app.get("/", checkAuth, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
 
 // Login Page [ http://localhost:3000/login ]
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuth, (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", async (req, res) => {});
+app.post(
+  "/login",
+  checkNotAuth,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 // Registration Page [ http://localhost:3000/register ]
 
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuth, (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
+app.post("/register", checkNotAuth, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
@@ -39,11 +74,40 @@ app.post("/register", async (req, res) => {
       password: hashedPassword,
     });
     res.redirect("/login");
-  } catch (err) {
+  } catch {
     res.redirect("/register");
   }
-  console.log(users);
 });
 
-app.listen(3000);
-console.log("Server is on");
+// Logout if Logged in
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+// Middlewares
+
+function checkAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+}
+
+function checkNotAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+// Server
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server is on port ${PORT}`);
+});
